925dd963f9d727966635b73758c29204a8a65ce4
heyxdelaram <djanfarsa@gmail.com>
2025-01-24 00:02:35 +0330

    reportslist ui

--- src/app/components/ReportsList.js
+++ src/app/components/ReportsList.js
@@ -14,26 +14,26 @@ const ReportsList = ({ setIsModalOpen }) => {
         </button>
       </div>
       <div>
-        <label
-          htmlFor="year"
-          className="block text-sm font-medium text-gray-600"
-        >
-          Year
-        </label>
-        <select
-          id="year"
-          className="w-full p-3 mb-4 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500"
-        >
-          <option>2025</option>
-          <option>2024</option>
-          <option>2023</option>
-        </select>
-        <div className="space-y-4">
-          <button className="w-full flex items-center justify-between bg-gray-100 p-4 rounded-xl text-gray-700 hover:bg-gray-200">
+        <div className="flex flex-col items-center">
+          {/* <label className="block w-28 text-m font-semibold text-gray-600">
+            Year
+          </label> */}
+          <select
+            id="billType"
+            name="billType"
+            className="w-full mb-8 p-4 border border-gray-300 bg-black rounded-xl focus:outline-none text-white text-sm focus:ring-2 focus:ring-green-500"
+          >
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+          </select>
+        </div>
+        <div className="space-y-4 text-white">
+          <button className="w-full  flex items-center justify-between p-4 bg-green-600 rounded-xl hover:bg-gray-200">
             <span>January</span>
             <span>11:22 PM</span>
           </button>
-          <button className="w-full flex items-center justify-between bg-gray-100 p-4 rounded-xl text-gray-700 hover:bg-gray-200">
+          <button className="w-full  flex items-center justify-between bg-green-400 p-4 rounded-xl hover:bg-gray-200">
             <span>March</span>
             <span>11:22 PM</span>
           </button>
